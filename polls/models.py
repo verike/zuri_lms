32456ca84fbfb7398d463e943c8bc88565a96ba1
Victor Erike <verike7@gmail.com>
2022-09-02 11:16:55 -0400

    polls: Update Polls model and make migrations

---
+++ polls/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=255)
+    question_description = models.CharField(max_length=255, default="Basic description")
+    pub_date = models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choices(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_answer = models.CharField(max_length=28)
+    vote = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_answer
